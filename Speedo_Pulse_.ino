1d10fcf15268bcda93a2a31d255abb9c8618d5b2
Ben Ford <benford_266@hotmail.com>
2021-08-09 21:20:53 +0100

    Update Speedo_Pulse_.ino

--- Speedo_Pulse_.ino
+++ Speedo_Pulse_.ino
@@ -1,18 +1,23 @@
-//GPS to Speedo Pulse
+// Project: GPS to Speedo Pulse
+// Author: Ben Ford
+// Use: Simulate vehicle speed pulse using GPS speed.
+// Arduino Mega2650 and NEO-6M
+
+
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
 
 // Setup modules
 TinyGPS gps;
-SoftwareSerial ss(8, 9); // RX / TX <<
+SoftwareSerial ss(64, 65); // RX / TX << pins to NEO-6M
 
 unsigned long cycleStart, period = 100000UL, onTime = period / 2;
 const byte outPin = 13; // Output Pin to ECU / Speedo
 
 void setup()
 {
-  Serial.begin(9600);
-  ss.begin(9600); // Begin serial comms 9600 baud
+  Serial.begin(9600); // Debug serial
+  ss.begin(9600); // Begin serial comms to GPS 9600 baud
   Serial.println();
   pinMode(outPin, OUTPUT);
 }
@@ -37,14 +42,21 @@ void loop()
   }
 
   // Pulse Generator
+  // Checks if speed is below 1mph if so no pulse is generated.
+  if (gps.f_speed_mph() < 1){
+    Serial.println("No Pulse");
+  }
+  else { // Above 1mph pulse is generated and 1Hz per MPH / 3600 pulses per Mile
   digitalWrite(outPin, micros() - cycleStart < onTime);
   if(micros() - cycleStart > period) // restart timer
     cycleStart += period; 
-  
-    int freq  = int(gps.f_speed_mph());
-    freq = constrain(freq, 1, 200);
-    period = 1000000 / freq; // 1 million / freq
-    onTime = period / 2; // duty cycle (100 / 2 = 50)
-    cycleStart = micros();
-    Serial.println(freq);
+     int freq  = int(gps.f_speed_mph());
+     Serial.println(gps.f_speed_mph());
+     freq = constrain(freq, 1, 200);
+     period = 1000000 / freq; // 1 million / freq
+     onTime = period / 2; // duty cycle (100 / 2 = 50)
+     cycleStart = micros();
+     Serial.println(freq);
+  }
+    
 }
