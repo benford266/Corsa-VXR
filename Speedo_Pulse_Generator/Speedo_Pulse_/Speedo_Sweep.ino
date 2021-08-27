a83a3ba62e2a26eefcc53310511788c3f535c011
Ben Ford <benford_266@hotmail.com>
2021-08-27 15:45:27 +0100

    VSS tester

---
+++ Speedo_Pulse_Generator/Speedo_Pulse_/Speedo_Sweep.ino
@@ -0,0 +1,22 @@
+// Project: Speedo Pulse Test 1 - 100Hz Pin 10
+// Author: Ben Ford
+// Use: Simulate vehicle speedo.
+// Arduino Pro Micro.
+
+int freq = 1;
+byte outpin = 10;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly
+  while(freq < 101){
+    Serial.println(freq);
+    tone(10,freq);
+    freq++;
+    delay(500);
+  }
+  freq = 1;
+}
